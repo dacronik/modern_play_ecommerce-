c6c6f58c365e27b97121e050075d770365b61ede
dacronik <dacronik@gmail.com>
2023-03-15 01:14:50 -0300

    agregando paginacion y busqueda de productos

--- store/views.py
+++ store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 from carts.models import CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -13,14 +15,20 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(
-            category=categories, is_available=True)
+            category=categories, is_available=True).order_by('id')
+        paginator = Paginator(products, 5)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 5)
+        page = request.GET.get('page')
+        page_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': page_products,
         'product_count': product_count
     }
     return render(request, 'store/store.html', context)
@@ -41,3 +49,19 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        Keyword = request.GET['keyword']
+        if Keyword:
+            products = Product.objects.order_by('-created_date').filter(
+                Q(description__icontains=Keyword) | Q(product_name__icontains=Keyword))
+            product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+
+    return render(request, 'store/store.html', context)
