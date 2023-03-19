c11f2f10a1c74d8dee668f514f6c6facd6025531
dacronik <dacronik@gmail.com>
2023-03-19 19:27:54 -0300

    configuracion de pago por paypal realizado

--- orders/views.py
+++ orders/views.py
@@ -3,9 +3,29 @@ from carts.models import CartItem
 from .forms import OrderForm
 from .models import Order
 import datetime
+from .models import  Order, Payment
+import json
+
 # Create your views here.
 
 def payments(request):
+    
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number = body['orderID'])
+    
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_id = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+    
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+    
     return render(request, 'orders/payments.html')
 
 def place_order(request, total =0, quantity =0):
@@ -18,6 +38,8 @@ def place_order(request, total =0, quantity =0):
     
     grand_total = 0
     tax = 0
+    total = 0
+    quantity = 0
     
     for cart_item in cart_items:
         total += (cart_item.product.price * cart_item.quantity)
@@ -55,7 +77,19 @@ def place_order(request, total =0, quantity =0):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
+            
+            order = Order.objects.get(user=current_user, is_ordered = False, order_number=order_number)
+           
+            context = {
+                'order':order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+            
+            return render(request, 'orders/payments.html', context)
         
         else:
             return redirect('checkout')
+
